3506cdff4b4f999da8100513e7dcdd8b16fb5bc8
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2019-01-23 01:57:16 +0700

    add grafana setup sh

--- build-os.sh
+++ build-os.sh
@@ -1,4 +1,3 @@
 docker build -t centos-sandbox . ;
-docker run -d -p 3000:3000 --privileged --name centos-grafana centos-sandbox ;
-docker run -d -p 8086:8086 --privileged --name centos-influxdb centos-sandbox ;
-docker exec -it os bash
+docker run -d -p 3000:3000 --rm --privileged --name centos-grafana centos-sandbox ;
+docker run -d -p 8086:8086 --rm --privileged --name centos-influxdb centos-sandbox
---
+++ grafana-centos-setup.sh
@@ -0,0 +1,9 @@
+yum -y install https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm ;
+
+echo -e "$(echo /etc/sysconfig/grafana-server)\n
+GF_AUTH_ANONYMOUS_ENABLED=true\n
+GF_SECURITY_ADMIN_USER="admin"\n
+GF_SECURITY_ADMIN_PASSWORD="pass"
+" > /etc/sysconfig/grafana-server);
+
+service grafana-server start
