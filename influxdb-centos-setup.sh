823f563380b66e19dbc64f7008b9eee344969c9f
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2019-01-23 02:52:25 +0700

    influxdb setup added

---
+++ influxdb-centos-setup.sh
@@ -0,0 +1,11 @@
+cat << EOF | tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL \$releasever
+baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
+EOF
+
+yum install -y influxdb ;
+systemctl start influxdb
