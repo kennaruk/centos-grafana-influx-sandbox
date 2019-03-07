569302b19b261b95c8a1d642e86779ca4204a033
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2019-03-07 19:43:52 +0900

    add test 1s

---
+++ influxdb-centos-setup-test.sh
@@ -0,0 +1,33 @@
+# install influxdb
+cat << EOF | tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL \$releasever
+baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
+EOF
+yum install -y influxdb ;
+systemctl start influxdb ;
+
+echo "sleep 1s after start influxdb"
+sleep 1s ;
+influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES" ;
+# create authentication user
+# echo "influx fire" ;
+# {
+# 	RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES") ;
+# } || {
+
+# }
+# echo "RESULT=$RESULT" ;
+# while [ "$RESULT" != "" ];
+# do
+# 	RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES")
+# done
+
+# enable authentication
+sed -i "s/# auth-enabled = false/auth-enabled = true/" /etc/influxdb/influxdb.conf ;
+
+# start service
+systemctl restart influxdb
