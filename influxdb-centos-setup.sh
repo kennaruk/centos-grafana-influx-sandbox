b1e331a0c1ee3fc78afa7d0bf90553df802af40c
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2019-03-07 15:48:24 +0900

    t

--- influxdb-centos-setup.sh
+++ influxdb-centos-setup.sh
@@ -9,13 +9,15 @@ gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
 yum install -y influxdb ;
 systemctl start influxdb ;
-
+sleep 1s ;
 # create authentication user
-RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES") > result_from_add_user.log ;
-while [ "$RESULT" != "" ] 
-do
-	RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES")
-done
+echo "influx fire" ;
+RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES") ;
+echo "RESULT=$RESULT" ;
+# while [ "$RESULT" != "" ];
+# do
+# 	RESULT=$(influx -execute "CREATE USER kenadmin WITH PASSWORD 'kenpassword' WITH ALL PRIVILEGES")
+# done
 
 # enable authentication
 sed -i "s/# auth-enabled = false/auth-enabled = true/" /etc/influxdb/influxdb.conf ;
